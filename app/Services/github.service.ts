cf19b079be494d4107f273d4d59c03fa31d2677e
Eran Krakovsky <eran@krakovsky.info>
2017-01-29 00:05:42 +0200

    first commit

---
+++ app/Services/github.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  private username: string;
+  private client_id = '7bb50ba1714b7488bc25';
+  private client_secret= '7bdfa8270e5d1646b16ae65ac39f514d24e9cdc5';
+
+  constructor(private _http: Http) {
+    console.log('Github Service Ready....');
+    this.username = 'ekrako';
+  }
+  getUser() {
+    return this._http.get('http://api.github.com/users/' + this.username
+    + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+      .map(res => res.json());
+  }
+  getRepos() {
+    return this._http.get('http://api.github.com/users/' + this.username
+    + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+      .map(res => res.json());
+  }
+}
---
+++ app/components/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {GithubService} from '../Services/github.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'profile',
+  templateUrl: `profile.component.html`
+})
+export class ProfileComponent  {
+  user: any[];
+  repos: any[];
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe(user => {
+     this.user = user;
+    });
+    this._githubService.getRepos().subscribe(repos => {
+     this.repos = repos;
+    });
+
+  }
+}
